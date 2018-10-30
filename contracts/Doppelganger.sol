f0a46579ed5a2fb74d3d4ef4e311ecbd80da0066
Antoni Kedracki <antoni@ethworks.io>
2018-10-30 13:02:49 +0100

    Even smaller and simpler Doppelganger contract. Version bump to 0.0.4.

--- contracts/Doppelganger.sol
+++ contracts/Doppelganger.sol
@@ -3,22 +3,16 @@ pragma solidity ^0.4.24;
 
 
 contract Doppelganger {
-    mapping (uint32 => bytes) mockConfig;
+    mapping (bytes4 => bytes) mockConfig;
     
     function() public {
-        uint32 key;
-        bytes memory ret;
-        assembly {
-            calldatacopy(0x0, 0x0, 0x4)
-            key := div(mload(0x0), 0x0000000100000000000000000000000000000000000000000000000000000000)
-        }
-        ret = mockConfig[key];
+        bytes memory ret = mockConfig[msg.sig];
         assembly {
             return(add(ret, 0x20), mload(ret))
         }
     }
 
-    function mockReturns(uint32 key, bytes value) public {
+    function mockReturns(bytes4 key, bytes value) public {
         mockConfig[key] = value;
     }
 }
